0a7a2e512c01f10752a1ce7927352cdf12c25a49
chaerin <chae040991@gmail.com>
2024-09-12 16:32:18 +0900

    Title: 최대로 겹치는 지점, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240912/최대로 겹치는 지점/maximum-overlapped-points.cpp
+++ 240912/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -1,22 +1,21 @@
 #include<iostream>
 using namespace std;
 
-#define OFFSET 100
-#define MAX 200
+#define MAX 200     // 구간 범위에 맞게 배열 크기를 넉넉히 설정
 
 int checked[MAX + 1]{ 0 };  // 배열 크기를 MAX + 1로 설정
 
 int main() {
     int n;
     cin >> n;
-    int x1[MAX];
-    int x2[MAX];
+    int x1[100];  // 구간 시작값 배열
+    int x2[100];  // 구간 끝값 배열
 
     // 구간 입력받기 및 처리
     for (int i{ 0 }; i < n; i++) {
         cin >> x1[i] >> x2[i];
 
-        // i++ 대신 j++로 수정하여 구간의 값들을 처리
+        // 시작 구간 x1[i]에서 끝 구간 x2[i]까지 값을 체크
         for (int j{ x1[i] }; j < x2[i]; j++) {
             checked[j]++;
         }
@@ -32,6 +31,7 @@ int main() {
             max_index = i;
         }
     }
+
     // 가장 많이 체크된 구간 출력
     cout << max;
 }
