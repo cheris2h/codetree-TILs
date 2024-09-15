5c5249225398cbbf255e052684f49d11cabe3a91
chaerin <chae040991@gmail.com>
2024-09-15 14:00:12 +0900

    Title: 왔다 갔던 구역 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240915/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int num{ 0 };
+	char direction;
+	int arr[2000]{ 0, };
+	int location{ 1000 };
+	for (int i{ 0 }; i < n; i++) {
+		cin >> num >> direction;
+		if (direction == 'R') {
+			for (int j{ location }; j < location + num; j++) {
+				arr[j]++;
+			}
+			location += num;
+		}
+		else if (direction == 'L') {
+			for (int j{ location }; j > location - num; j--) {
+				arr[j]++;
+			}
+			location -= num;
+		}
+	}
+		int sum{ 0 };
+		for (int i{ 0 }; i < 2000; i++) {
+			if (arr[i] >= 2) {
+				sum++;
+			}
+		}
+		cout << sum;
+}
